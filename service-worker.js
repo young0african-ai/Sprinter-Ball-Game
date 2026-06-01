f37b5c2c522ccb82288238d05fd4516a76ead8c8
young0african-ai <young0african@gmail.com>
2026-06-01 14:48:53 -0600

    Improve landscape mobile controls

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "sprinter-ball-v29";
+const CACHE_NAME = "sprinter-ball-v30";
 const FILES = [
   "./",
   "./index.html",
-  "./src/styles.css?v=29",
-  "./src/game.js?v=29",
+  "./src/styles.css?v=30",
+  "./src/game.js?v=30",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./README.md"
