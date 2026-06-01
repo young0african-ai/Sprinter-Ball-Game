78f88b4bca4552d7081a67586d9ce501246d494b
young0african-ai <young0african@gmail.com>
2026-06-01 12:15:28 -0600

    Improve mobile game layout

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "sprinter-ball-v16";
+const CACHE_NAME = "sprinter-ball-v17";
 const FILES = [
   "./",
   "./index.html",
-  "./src/styles.css?v=16",
-  "./src/game.js?v=16",
+  "./src/styles.css?v=17",
+  "./src/game.js?v=17",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./README.md"
